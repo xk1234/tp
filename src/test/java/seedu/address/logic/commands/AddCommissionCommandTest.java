988ff452536a831a3c5daaa9a0d5cd822fcfc542
Bilguudei <bilguudei.shinezul23@gmail.com>
2025-03-13 19:45:24 +0800

    Fix testing

--- src/test/java/seedu/address/logic/commands/AddCommissionCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommissionCommandTest.java
@@ -1,18 +1,49 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.address.testutil.Assert.assertThrows;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_COMMISSION_HUNDRED;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Commission;
 
 public class AddCommissionCommandTest {
 
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        Commission commission = new Commission(VALID_COMMISSION_HUNDRED);
+        AddCommissionCommand addCommissionCommand = new AddCommissionCommand(outOfBoundIndex, commission);
+
+        assertCommandFailure(addCommissionCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
     @Test
     public void constructor_nullIndex_throwsNullPointerException() {
-        Commission validCommission = new Commission("100");
+        Commission validCommission = new Commission(VALID_COMMISSION_HUNDRED);
         assertThrows(NullPointerException.class, () ->
                 new AddCommissionCommand(null, validCommission));
     }
@@ -27,9 +58,54 @@ public class AddCommissionCommandTest {
     @Test
     public void constructor_validArguments_success() {
         Index validIndex = Index.fromOneBased(1);
-        Commission validCommission = new Commission("100");
+        Commission validCommission = new Commission(VALID_COMMISSION_HUNDRED);
         AddCommissionCommand command = new AddCommissionCommand(validIndex, validCommission);
 
         assertEquals(command, new AddCommissionCommand(validIndex, validCommission));
+    }
+
+    @Test
+    public void execute_commandSuccess() {
+        Index validIndex = Index.fromOneBased(1);
+        Commission validCommission = new Commission(VALID_COMMISSION_HUNDRED);
+        assertCommandSuccess(new AddCommissionCommand(validIndex, validCommission),
+                model, AddCommissionCommand.MESSAGE_ADD_COMMISSION_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        Index validIndex = Index.fromOneBased(1);
+        Index validIndex2 = Index.fromOneBased(2);
+        Commission validCommission = new Commission(VALID_COMMISSION_HUNDRED);
+
+        AddCommissionCommand firstCommand = new AddCommissionCommand(validIndex, validCommission);
+        // same object -> return true
+        assertTrue(firstCommand.equals(firstCommand));
+
+        // same values -> return true
+        assertTrue(firstCommand.equals(new AddCommissionCommand(validIndex, validCommission)));
+
+        // different types -> returns false
+        assertFalse(firstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(firstCommand.equals(null));
+
+        // different commissions -> return false
+        assertFalse(firstCommand.equals(new AddCommissionCommand(validIndex2, validCommission)));
+    }
+
+    @Test
+    public void toStringMethod() {
+        Index validIndex = Index.fromOneBased(1);
+        Commission validCommission = new Commission(VALID_COMMISSION_HUNDRED);
+        AddCommissionCommand command = new AddCommissionCommand(validIndex, validCommission);
+        String expectedString = new ToStringBuilder(command)
+                .add("index", validIndex)
+                .add("commission", validCommission)
+                .toString();
+
+        assertEquals(expectedString, command.toString());
+
     }
 }
--- src/test/java/seedu/address/logic/commands/TotalCommandTest.java
+++ src/test/java/seedu/address/logic/commands/TotalCommandTest.java
@@ -1,7 +1,12 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +14,8 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Commission;
+import seedu.address.model.person.Person;
 
 public class TotalCommandTest {
 
@@ -24,5 +31,25 @@ public class TotalCommandTest {
     @Test
     public void execute_totalIsNotFiltered_showsSameValue() {
         assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    void calculateTotalCommission_correctlySumsValues() {
+        List<Person> lastShownList = List.of(
+                ALICE,
+                BENSON
+        );
+
+        Commission totalCommission = lastShownList.stream()
+                .map(Person::getCommission)
+                .reduce(new Commission("0"), Commission::addValue);
+
+        assertEquals(new Commission("0"), totalCommission);
+    }
+
+    @Test
+    public void execute_commandSuccess() {
+        assertCommandSuccess(new TotalCommand(),
+                model, TotalCommand.MESSAGE_TOTAL_SUCCESS, expectedModel);
     }
 }
