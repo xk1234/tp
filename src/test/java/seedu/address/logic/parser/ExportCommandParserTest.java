c9a882b3d7f763885fbc2ac6afe211919f0fb49a
xumarcus <marcus.xu@u.nus.edu>
2025-03-15 11:48:40 +0800

    Use PREFIX_ATTRIBUTE in test

--- src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ATTRIBUTE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
@@ -18,27 +19,32 @@ public class ExportCommandParserTest {
     @Test
     public void parse_validCaseInsensitiveAttribute_success() {
         List<Attribute> expectedAttributes = List.of(Attribute.PHONE);
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + " a/pHoNe",
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + " " + PREFIX_ATTRIBUTE + "pHoNe",
                 new ExportCommand(expectedAttributes));
     }
 
     @Test
     public void parse_validMultipleAttributes_success() {
         List<Attribute> expectedAttributes = List.of(Attribute.NAME, Attribute.EMAIL);
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + " a/NAME a/EMAIL",
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE
+                        + " " + PREFIX_ATTRIBUTE + "NAME"
+                        + " " + PREFIX_ATTRIBUTE + "EMAIL",
                 new ExportCommand(expectedAttributes));
     }
 
     @Test
     public void parse_repeatedAttribute_failure() {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE);
-        assertParseFailure(parser, PREAMBLE_WHITESPACE + " a/name a/name", expectedMessage);
+        assertParseFailure(parser, PREAMBLE_WHITESPACE
+                + " " + PREFIX_ATTRIBUTE + "name"
+                + " " + PREFIX_ATTRIBUTE + "name", expectedMessage);
     }
 
     @Test
     public void parse_invalidAttribute_failure() {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE);
-        assertParseFailure(parser, PREAMBLE_WHITESPACE + " a/invalid", expectedMessage);
+        assertParseFailure(parser, PREAMBLE_WHITESPACE
+                + " " + PREFIX_ATTRIBUTE + "invalid", expectedMessage);
     }
 
     @Test
