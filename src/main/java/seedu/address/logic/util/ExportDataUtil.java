41d56b90081ab2e94a351060b05177b63c735ada
xumarcus <marcus.xu@u.nus.edu>
2025-03-23 19:57:33 +0800

    Refactor code into Person class

--- src/main/java/seedu/address/logic/util/ExportDataUtil.java
+++ src/main/java/seedu/address/logic/util/ExportDataUtil.java
@@ -18,17 +18,6 @@ import seedu.address.model.person.Person;
  * Contains utility methods for exporting data from {@code AddressBook}.
  */
 public class ExportDataUtil {
-    // We have to use Object here since none of name, email, etc. have a common interface
-    private static final EnumMap<Attribute, Function<Person, Object>> PERSON_METHOD_MAP =
-            new EnumMap<>(Attribute.class);
-    static {
-        PERSON_METHOD_MAP.put(Attribute.NAME, Person::getName);
-        PERSON_METHOD_MAP.put(Attribute.PHONE, Person::getPhone);
-        PERSON_METHOD_MAP.put(Attribute.EMAIL, Person::getEmail);
-        PERSON_METHOD_MAP.put(Attribute.ADDRESS, Person::getAddress);
-        PERSON_METHOD_MAP.put(Attribute.COMMISSION, Person::getCommission);
-    }
-
     /**
      * Exports the selected attributes of all persons in the given model to a CSV file at the specified path.
      * The CSV file will have a header row containing the names of the attributes, followed by rows
@@ -52,9 +41,7 @@ public class ExportDataUtil {
             // CSV body
             for (Person person : model.getFilteredPersonList()) {
                 writer.write(attributes.stream()
-                        .map(attribute -> PERSON_METHOD_MAP.get(attribute)
-                                .apply(person)
-                                .toString())
+                        .map(attribute -> person.getAttribute(attribute).toString())
                         .collect(Collectors.joining(",")));
                 writer.newLine();
             }
