82d6da2e5505472f8824f89ed9f6ed7a6c604de4
xumarcus <51620147+xumarcus@users.noreply.github.com>
2025-04-07 00:02:14 +0800

    Update src/main/java/seedu/address/commons/util/StringUtil.java
    Co-authored-by: BladerX11 <134282326+BladerX11@users.noreply.github.com>

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -53,7 +53,7 @@ public class StringUtil {
     }
 
     /**
-     * Returns BigInteger if {@code s} represents a non-zero unsigned integer
+     * Returns Integer if {@code s} represents a non-zero unsigned integer
      * e.g. 1, 2, 3, ..., <br>
      * Will return false for any other non-null string input
      * e.g. empty string, "-1", "0", "+1", and " 2 " (untrimmed), "3 0" (contains whitespace), "1 a" (contains letters)
